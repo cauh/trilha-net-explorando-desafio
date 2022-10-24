1283163691ddc1e3f4b390369fc0e82664f5b633
Claudia Hirose <claudiahirose@gmail.com>
2022-10-24 20:40:56 -0300

    Desafio 'Construindo um sistema de hospedagem de um hotel no C#' concluído

--- Program.cs
+++ Program.cs
@@ -16,10 +16,54 @@ hospedes.Add(p2);
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
+
+// Reserva 1:
+// Dias reservados: 5
+// Hóspdes: 2
+// Valor diária: 150,00
+
+Console.WriteLine("Reserva 1:");
+
 Reserva reserva = new Reserva(diasReservados: 5);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"Dias reservados: {reserva.DiasReservados}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria():C}");
+
+// Reserva 2:
+// Dias reservados: 5
+// Hóspdes: 2
+// Valor diária: 150,00
+
+Console.WriteLine("\nReserva 2:");
+
+reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Dias reservados: {reserva.DiasReservados}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria():C}");
+
+
+// Reserva 3:
+// Dias reservados: 5
+// Hóspdes: 2
+// Capacidade da suíte = 1
+// Gera uma exceção de capacidade de hóspedes que não comporta na suíte
+
+Console.WriteLine("\nReserva 3:");
+
+reserva = new Reserva(diasReservados: 10);
+suite.Capacidade = 1;
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Dias reservados: {reserva.DiasReservados}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria():C}");
